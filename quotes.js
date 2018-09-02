37f63c0aac482c85b272b863fc80cf5badcbb0e3
Puja Ramanathan <puja.ramanathan@gmail.com>
2018-09-02 00:10:49 -0700

    Finished Education panel

--- quotes.js
+++ quotes.js
@@ -8,17 +8,13 @@ This JavaScript file fetches the quote of the day from Quotes REST API.
     var author;
     var blockquote;
 
-    window.onload = function() {
-        var img = document.getElementsByClassName("bg-img")[0];
-        console.log(img.offsetHeight);
-        var header = document.getElementsByTagName("header")[0];
-        console.log(header.offsetHeight);
-
+    window.onload = function () {
         blockquote = document.getElementById("quote");
         getQuote();
     };
 
-    getQuote();
+    // blockquote = document.getElementById("quote");
+    // getQuote();
 
     function getQuote() {
         var url = "http://quotes.rest/qod.json";
@@ -27,7 +23,7 @@ This JavaScript file fetches the quote of the day from Quotes REST API.
             .then(checkStatus)
             .then(JSON.parse)
             .then(fillInQuote)
-            .catch(function(error) {
+            .catch(function (error) {
                 quote = "The best thing about a boolean is even if you are wrong, you are only off by a bit.";
                 author = "Anonymous";
                 blockquote.innerText = "\"" + quote + "\"";
@@ -41,16 +37,6 @@ This JavaScript file fetches the quote of the day from Quotes REST API.
         author = response.contents.quotes[0].author;
     }
 
-    function getQOD() {
-        blockquote.style.color = "black";
-        blockquote.innerText = "\"" + quote + "\"";
-    }
-
-    function getAuthor() {
-        blockquote.style.color = "gray";
-        blockquote.innerText = "- " + author;
-    }
-
     // Checks status of JSON 'response'
     // Returns 'response' text if valid,
     // otherwise returns the error status
@@ -61,6 +47,18 @@ This JavaScript file fetches the quote of the day from Quotes REST API.
             return Promise.reject(new Error(response.status + ": " + response.statusText));
         }
     }
+
+    function getQOD() {
+        blockquote.style.color = "black";
+        blockquote.innerText = "\"" + quote + "\"";
+    }
+
+    function getAuthor() {
+        blockquote.style.color = "gray";
+        blockquote.innerText = "- " + author;
+    }
+
+
 // })();
 
 
